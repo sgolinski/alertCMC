9e91f6c7f6622e6a2daf6fee077fbfb5c5e04e8a
sgolinski <simon.golinski@gmail.com>
2022-03-28 00:18:09 +0200

    first commit

---
+++ src/Crawler.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace alertCMC;
+
+use ArrayIterator;
+use Exception;
+use Facebook\WebDriver\Remote\RemoteWebElement;
+use Facebook\WebDriver\WebDriverBy;
+use Symfony\Component\Panther\Client as PantherClient;
+
+class Crawler
+{
+    private PantherClient $client;
+    public $returnArray;
+
+    public function __construct()
+    {
+        $this->client = PantherClient::createChromeClient();
+        $this->returnArray = [];
+
+    }
+
+    public function assignDetailInformationToCoin($link)
+    {
+        $this->client->refreshCrawler();
+        $this->client->get($link);
+        $percent = $this->client->getCrawler()
+            ->filter('div.sc-16r8icm-0.kjciSH.priceTitle')
+            ->getText();
+        $this->client->quit();
+        return $percent;
+    }
+
+    public static function removeDuplicates($arr1, $arr2)
+    {
+        $uniqueArray = [];
+        $notUnique = false;
+        if (!empty($arr2)) {
+            foreach ($arr1 as $coin) {
+                $notUnique = false;
+                foreach ($arr2 as $coin2) {
+                    if ($coin == $coin2) {
+                        $notUnique = true;
+                    }
+                }
+                if (!$notUnique) {
+                    $uniqueArray[] = $coin;
+                }
+            }
+            return $uniqueArray;
+        } else {
+            return $arr1;
+        }
+    }
+}
