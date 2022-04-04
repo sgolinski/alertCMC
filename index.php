ea9680fb8227c9c09dba6d06c9bb458487775cad
sgolinski <simon.golinski@gmail.com>
2022-04-04 21:43:44 +0200

    refactor

--- index.php
+++ index.php
@@ -14,45 +14,51 @@ $serializedList = unserialize($serializedList);
 
 $crawler = new Crawler();
 $slack = new SlackClient('https://hooks.slack.com/services/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');;
+$lastRoundCoins = null;
 
-$lastRoundCoins = file_get_contents('last_rounded_coins.txt');
-
+try {
+    $lastRoundCoins = file_get_contents('last_rounded_coins.txt');
+} catch (Exception $e) {
+    echo $e->getMessage() . PHP_EOL;
+}
 if (empty($lastRoundCoins)) {
     $lastRoundCoins = [];
 } else {
-    unserialize($lastRoundCoins);
-    shuffle($arr);
+    $lastRoundCoins = unserialize($lastRoundCoins);
+    shuffle($serializedList);
 }
 
 foreach ($serializedList as $coin) {
     try {
         assert($coin instanceof Coin);
-        $data = $crawler->assignDetailInformationToCoin($coin->getCmcLink());
-        if ($data[0]) {
-            $percent = '-' . explode("\n", $data[1])[1];
-        } else {
-            $percent = explode("\n", $data[1])[1];
+        if (!array_search($coin->getName(), $lastRoundCoins)) {
+            $data = $crawler->assignDetailInformationToCoin($coin->getCmcLink());
+            if ($data[0]) {
+                $percent = '-' . explode("\n", $data[1])[1];
+            } else {
+                $percent = explode("\n", $data[1])[1];
+            }
+            $percent = floatval($percent);
+            if ($percent < -30.0) {
+                $coin->setPercent($percent);
+                $message = new Message();
+                $message->setText($coin->getDescription());
+                $slack->sendMessage($message);
+                $crawler->returnArray[] = $coin;
+            }
         }
-        $percent = floatval($percent);
-        if ($percent < -30.0) {
-            $coin->setPercent($percent);
-            $crawler->returnArray[] = $coin;
-        }
-
-    } catch (Exception $e) {
+    } catch
+    (Exception $e) {
         $crawler->getClient()->quit();
         continue;
     }
-}
-$alertCoins = Crawler::removeDuplicates($crawler->returnArray, $lastRoundCoins);
 
-foreach ($alertCoins as $coin) {
-    assert($coin instanceof Coin);
-    $message = new Message();
-    $message->setText($coin->getDescription());
-    $slack->sendMessage($message);
+}
+$crawler->getClient()->quit();
+
+if (count($crawler->returnArray) > 0) {
+    file_put_contents('last_rounded_coins.txt', serialize($crawler->returnArray));
 }
 
-file_put_contents('last_rounded_coins.txt', serialize($alertCoins));
 
 
